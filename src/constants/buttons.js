d05d1746a39ef082b860ede714a115f3aed002df
lijing <1023283537@qq.com>
2022-02-15 10:16:10 +0800

    feat: add buttons

---
+++ src/constants/buttons.js
@@ -0,0 +1,32 @@
+// 盲盒
+export const BLIND_STATUS = {
+  soldOut: {
+    text: "售罄",
+  },
+  purchase: {
+    text: "购买",
+  },
+};
+
+// Market old
+export const MARKET_STATUS = {
+  soldOut: {
+    text: "售罄",
+  },
+  purchase: {
+    text: "购买",
+  },
+  bidPrice: {
+    text: "出价",
+  },
+};
+
+// BuyBack  => 平台回购按钮状态 (需要出售弹窗)
+export const BUYBACK_STATUS = {
+  sell: {
+    text: "出售",
+  },
+};
+
+// My NFT
+export const MyNft = {};
