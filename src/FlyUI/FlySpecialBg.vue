7ca011fa44a1f23a801aca8ea8732156e45142c2
xiaziheng <xzhmhh@163.com>
2022-03-10 18:20:25 +0800

    feat:注释

--- src/FlyUI/FlySpecialBg.vue
+++ src/FlyUI/FlySpecialBg.vue
@@ -24,6 +24,7 @@ import SvgIcon from "@components/SvgIcon/Index.vue";
 import { debounce } from "lodash";
 const props = defineProps({
   showSquare: {
+    //是否显示方块，默认不显示
     type: Boolean,
     default: () => false,
   },
