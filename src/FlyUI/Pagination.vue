74caa49337f9a8527b484dfcb1b04449650a79ea
xiaziheng <xzhmhh@163.com>
2022-03-11 14:12:15 +0800

    feat : 分页

--- src/FlyUI/Pagination.vue
+++ src/FlyUI/Pagination.vue
@@ -1,65 +1,81 @@
 <!-- pagination -->
 <template>
-  <div>
+  <!-- :class="$style['white-list-tool']" -->
+  <!-- eslint-disable vue/no-deprecated-dollar-listeners-api -->
+  <div class="white-list-tool">
     <ElPagination
-      class="white-list-tool"
       background
       hide-on-single-page
       :page-size="10"
       layout="prev, pager, next"
-      :total="50"
-      prev-text="上一页"
-      next-text="下一页"
+      :prev-text="$t('上一页')"
+      :next-text="$t('下一页')"
+      v-bind="$attrs"
     ></ElPagination>
   </div>
 </template>
 
 <script setup></script>
 
-<style>
-.white-list-tool.is-background .btn-prev {
-  width: 48px;
-  background-color: rgba(255, 255, 255, 0.09);
-  color: #fff;
-}
-.white-list-tool.is-background .btn-prev:disabled {
-  color: rgba(255, 255, 255, 0.5);
-}
-.white-list-tool.is-background .btn-next {
-  width: 48px;
-  background-color: rgba(255, 255, 255, 0.09);
-  color: #fff;
-}
-.white-list-tool.is-background .btn-next:disabled {
-  background-color: rgba(255, 255, 255, 0.09);
-  color: rgba(255, 255, 255, 0.5);
-}
-.white-list-tool.is-background .el-pager li {
-  background-color: rgba(255, 255, 255, 0.09);
-  color: rgba(255, 255, 255, 0.5);
-}
-.white-list-tool.is-background .el-pager li:not(.disabled).active {
-  background-color: rgba(255, 255, 255, 0.09);
-  color: #fff;
-}
-.white-list-tool.is-background .el-pager li.active {
-  color: #fff;
-  cursor: default;
-}
-.white-list-tool.is-background .el-pager li:hover {
-  color: #fff;
-}
-.white-list-tool.is-background .el-pager li:not(.disabled):hover {
-  color: #fff;
-}
-.white-list-tool.is-background .el-pager li:not(.disabled).active:hover {
-  background-color: rgba(255, 255, 255, 0.09);
-  color: #fff;
-}
-.white-list-tool.is-background .btn-next .el-icon-arrow-right {
-  margin: 0 auto;
-}
-.white-list-tool.is-background .btn-prev .el-icon-arrow-left {
-  margin: 0 auto;
+<style scoped lang="scss">
+.white-list-tool {
+  ::v-deep(.el-pagination) {
+    background-color: rgba(255, 255, 255, 0.09);
+    padding: 6px;
+    .btn-prev {
+      width: 48px;
+      background: none;
+      color: rgba(255, 255, 255, 0.5);
+      // color: #fff;
+      &:not(:disabled):hover {
+        background-color: rgba(255, 255, 255, 0.2);
+      }
+    }
+    .btn-prev:disabled {
+      background: none;
+    }
+    .btn-next {
+      width: 48px;
+      background: none;
+      color: rgba(255, 255, 255, 0.5);
+      // color: #fff;
+      &:not(:disabled):hover {
+        background-color: rgba(255, 255, 255, 0.2);
+      }
+    }
+    .btn-next:disabled {
+      background: none;
+      // color: rgba(255, 255, 255, 0.5);
+    }
+    .el-pager li {
+      background: none;
+      color: rgba(255, 255, 255, 0.5);
+
+      &:not(.disabled) {
+        &.active {
+          background: none;
+          color: #fff;
+        }
+        &:hover {
+          background-color: rgba(255, 255, 255, 0.2);
+          color: #fff;
+        }
+        &.active:hover {
+          color: #fff;
+        }
+      }
+      &.active {
+        color: #fff;
+        cursor: default;
+      }
+    }
+
+    .btn-next .el-icon-arrow-right {
+      margin: 0 auto;
+    }
+    .btn-prev .el-icon-arrow-left {
+      margin: 0 auto;
+    }
+  }
 }
 </style>
