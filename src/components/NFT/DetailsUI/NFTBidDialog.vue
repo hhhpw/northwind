0b2e7793728af0bb15ad7f68a7af0881e7cf9814
webAmber <1023283537@qq.com>
2022-03-11 17:09:22 +0800

    feat: add bid-dialog ui

--- src/components/NFT/DetailsUI/NFTBidDialog.vue
+++ src/components/NFT/DetailsUI/NFTBidDialog.vue
@@ -331,9 +331,9 @@ const changeAction = (i) => {
       .bid-input-box {
         width: 372px;
         display: inline-block;
-        border-radius: 8px;
         .bid-price-content-box {
           background: rgba(255, 255, 255, 0.1);
+          border-radius: 8px;
         }
         .bid-price-input {
           width: 83%;
